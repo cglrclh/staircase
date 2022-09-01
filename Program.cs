9c92a370e4a6eeaba412e45a12e69c61d18dcb36
cglrclh <caglarculha35@gmail.com>
2022-09-02 02:20:22 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {  
+            Console.WriteLine(new String(' ',n-i)+new String('#',i));
+        }
+    }
+}
